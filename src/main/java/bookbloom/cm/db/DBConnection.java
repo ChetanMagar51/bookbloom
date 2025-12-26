14fc27669e8a908cf0fe7458ba44fd910f584456
chetan magar <sonumagar7264@gmail.com>
2025-12-26 20:10:00 +0530

    update -commit change

--- src/main/java/bookbloom/cm/db/DBConnection.java
+++ src/main/java/bookbloom/cm/db/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USER = "ebook"; // Change to your MySQL username
-    private static final String PASSWORD = "book"; // Change to your MySQL password
+    private static final String USER = "ebook"; // Change to your SQL username
+    private static final String PASSWORD = "book"; // Change to your SQL password
 
     public static Connection getConnection() {
         Connection conn = null;
