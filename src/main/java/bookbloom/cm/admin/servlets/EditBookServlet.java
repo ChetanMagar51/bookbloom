c6912db40d3bfff963aabe3716e0c2d5b2faa6ec
chetan magar <sonumagar7264@gmail.com>
2025-12-28 17:05:59 +0530

    update EditBookServlet change path

--- src/main/java/bookbloom/cm/admin/servlets/EditBookServlet.java
+++ src/main/java/bookbloom/cm/admin/servlets/EditBookServlet.java
@@ -2,6 +2,7 @@ package bookbloom.cm.admin.servlets;
 
 
 
+import java.io.File;
 import java.io.IOException;
 
 import bookbloom.cm.dao.BookDAO;
@@ -11,6 +12,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 @SuppressWarnings("serial")
 @WebServlet("/admin/EditBookServlet")
@@ -22,14 +24,32 @@ public class EditBookServlet extends HttpServlet {
         String title = request.getParameter("title");
         String author = request.getParameter("author");
         double price = Double.parseDouble(request.getParameter("price"));
-        String imageUrl = request.getParameter("imageUrl"); 
+//        String imageUrl = request.getParameter("imageUrl"); 
+        
+        
+        
+        Part imagePart = request.getPart("image");
+        String imageName = imagePart.getSubmittedFileName();
+
+        // Path inside project
+        String uploadPath = getServletContext().getRealPath("/images");
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+
+        // Save image physically
+        imagePart.write(uploadPath + File.separator + imageName);
+
+        // Store relative path in DB
+        String imagePath = "images/" + imageName;
 
         Book book = new Book();
         book.setId(id);
         book.setTitle(title);
         book.setAuthor(author);
         book.setPrice(price);
-        book.setImageUrl(imageUrl);
+        book.setImageUrl(imagePath);
 
         BookDAO bookDAO = new BookDAO();
         boolean updated = bookDAO.updateBook(book);
